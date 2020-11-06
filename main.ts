015b77717b202f7f66d9b849a43615081dd455ac
LuisFe Gomez <50106079+luisfegomezPE@users.noreply.github.com>
2020-11-06 18:07:02 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,48 @@
-let a = 0
-let n = 0
-if (n > 0 || n == 0) {
-    a = 1
-} else {
-    a = -1
+function aprendizaje () {
+    w1 += error * s1
+    w2 += error * s2
+    bias += error
 }
-basic.forever(function () {
-	
-})
+function procesamiento () {
+    sumaponderada = s1 * w1 + s2 * w2
+    n = sumaponderada + (0 + 0)
+    if (n > 0 || n == 0) {
+        a = 1
+    } else {
+        a = -1
+    }
+}
+let n = 0
+let sumaponderada = 0
+let contador = 0
+let a = 0
+let error = 0
+let target = 0
+let s2 = 0
+let s1 = 0
+let bias = 0
+let w2 = 0
+let w1 = 0
+if (input.buttonIsPressed(Button.A)) {
+    w1 = 0.25
+    w2 = 0.35
+    bias = -0.5
+    for (let index = 0; index < 12; index++) {
+        s1 = pins.digitalReadPin(DigitalPin.P0)
+        s2 = pins.digitalReadPin(DigitalPin.P1)
+        procesamiento()
+        target = pins.digitalReadPin(DigitalPin.P2)
+        error = target - a
+        contador = contador + 1
+        aprendizaje()
+    }
+}
+if (input.buttonIsPressed(Button.AB)) {
+    w1 = 0.25
+    w2 = 0.35
+    bias = -0.5
+    s1 = pins.digitalReadPin(DigitalPin.P0)
+    s2 = pins.digitalReadPin(DigitalPin.P1)
+    basic.showNumber(a)
+    basic.pause(2000)
+}
